1f03b30ca4c893919d245eba270490a9045b315a
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-10-30 00:56:18 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -54,6 +54,7 @@ const options = {
 flatpickr(inputEl, options);
 
 
+
 // import flatpickr from "flatpickr";
 // import "flatpickr/dist/flatpickr.min.css";
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
