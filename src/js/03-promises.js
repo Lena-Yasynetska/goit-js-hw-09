de6d973db02fe636c34c440d738766e226339175
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-10-30 21:57:28 +0100

    9/34

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,3 @@
-// const form = document.querySelector('.form');
-
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
-
-// form.addEventListener('submit', async function (event) {
-//   event.preventDefault();
-
-//   let delay = parseInt(form.querySelector('[name="delay"]').value);
-//   const step = parseInt(form.querySelector('[name="step"]').value);
-//   const amount = parseInt(form.querySelector('[name="amount"]').value);
-
-//   for (let i = 1; i <= amount; i++) {
-//     createPromise(i, delay)
-//       .then(({ position, delay }) => {
-//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       })
-//       .catch(({ position, delay }) => {
-//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
-
-//     delay += step;
-//   }
-// });
-
 
 
 // import throttle  from "lodash.throttle";
