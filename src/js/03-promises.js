2413ac8eb98a9e879e2d2d3510c164bc09ccf0fd
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-10-30 21:13:57 +0100

    9/30

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,93 @@
-// import throttle  from "lodash.throttle";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('.form');
 
-const userDate = {};
-
-const formEl = document.querySelector('.form');
-
-formEl.addEventListener('input', throttle(onInput, 500));
-formEl.addEventListener('submit', onSubmit);
-
-//-------function for submit button----------
-function onSubmit(event) {
-event.preventDefault();
-
-for (let i = 0; i < userDate.amount; i += 1){
-const stepDelay = userDate.delay + userDate.step * i;
-
-createPromise(i + 1, stepDelay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
-}
-//-------------------------------------------
-function createPromise(positionDate, delayDate) {
-  return new Promise((resolve, reject) => {
-    setTimeout((delay) => {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-  resolve({position: positionDate, delay: delayDate})
-  } else {
-    reject({position: positionDate, delay: delayDate})
-  }
-}, delayDate)
-})
-}
 
-function onInput(event) {
-if (event.target.name === 'delay') {
-    userDate.delay = +(event.target.value);
-}
-if (event.target.name === 'step') {
-  userDate.step = +(event.target.value);
-}
-if (event.target.name === 'amount') {
-  userDate.amount = +(event.target.value);
-}
-}
+form.addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  let delay = parseInt(form.querySelector('[name="delay"]').value);
+  const step = parseInt(form.querySelector('[name="step"]').value);
+  const amount = parseInt(form.querySelector('[name="amount"]').value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+  }
+});
+
+
+
+// import throttle  from "lodash.throttle";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const userDate = {};
+
+// const formEl = document.querySelector('.form');
+
+// formEl.addEventListener('input', throttle(onInput, 500));
+// formEl.addEventListener('submit', onSubmit);
+
+// //-------function for submit button----------
+// function onSubmit(event) {
+// event.preventDefault();
+
+// for (let i = 0; i < userDate.amount; i += 1){
+// const stepDelay = userDate.delay + userDate.step * i;
+
+// createPromise(i + 1, stepDelay)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+// }
+// }
+// //-------------------------------------------
+// function createPromise(positionDate, delayDate) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout((delay) => {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//   resolve({position: positionDate, delay: delayDate})
+//   } else {
+//     reject({position: positionDate, delay: delayDate})
+//   }
+// }, delayDate)
+// })
+// }
+
+// function onInput(event) {
+// if (event.target.name === 'delay') {
+//     userDate.delay = +(event.target.value);
+// }
+// if (event.target.name === 'step') {
+//   userDate.step = +(event.target.value);
+// }
+// if (event.target.name === 'amount') {
+//   userDate.amount = +(event.target.value);
+// }
+// }
 
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // //import { throttle }  from "lodash";
